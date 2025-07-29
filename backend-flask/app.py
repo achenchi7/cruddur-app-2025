dc978c116531c49da800490320ed9a3e0635f7be
Jully Achenchi <jullyachenchi8@gmail.com>
2025-07-29 14:22:55 +0000

    Additional configuration in client side

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -111,15 +111,13 @@ def data_home():
     # Authenticated request
     app.logger.debug('authenticated')
     app.logger.debug(claims)
+    app.logger.debug(claims['username'])
+    data = HomeActivities.run(cognito_user_id=claims['username'])
   except TokenVerifyError as e:
     # unauthenticated request
     app.logger.debug(e)
     app.logger.debug('unauthenticated')
-  
- 
-
-  data = HomeActivities.run()
-  
+    data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 
 class HomeActivities:
-  def run():
+  def run(cognito_user_id=None):
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
@@ -40,5 +40,19 @@ class HomeActivities:
       'expires_at': (now + timedelta(hours=12)).isoformat(),
       'likes': 0,
       'replies': []
-    }]
+    }
+    ]
+
+    if cognito_user_id != None:
+      extra_crud = {
+      'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+      'handle':  'lore',
+      'message': 'My dear brother, you are the problem',
+      'created_at': (now - timedelta(hours=1)).isoformat(),
+      'expires_at': (now + timedelta(hours=12)).isoformat(),
+      'likes': 1000,
+      'replies': []
+      }
+      results.insert(0,extra_crud)
+
     return results
